a9794df6a082c1e9a42362b8c839d9e0d8cbf67c
GuilhermeGDGP <guilhermedgoncalvez@gmail.com>
2020-07-13 15:21:51 -0300

    Removendo dados

--- App.py
+++ App.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
+app = Flask(__name__)
+
 data = []
-with open('configdb.txt') as load:
+with open('config.txt') as load:
     for line in load:
         data.append(line.strip())
 
-app = Flask(__name__)
 app.config['MYSQL_HOST'] = data[0]
 app.config['MYSQL_USER'] = data[1]
 app.config['MYSQL_PASSWORD'] = data[2]
 app.config['MYSQL_DB'] = data[3]
 mysql = MySQL(app)
 
+app.secret_key = data[4]
+
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    datadb = cur.fetchall()
+    return render_template('index.html', contacts=datadb)
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
@@ -26,15 +32,20 @@ def add_contact():
         cur = mysql.connection.cursor()
         cur.execute(f'INSERT INTO contacts (name, phone, email) VALUES ("{name}", "{phone}", "{email}");')
         mysql.connection.commit()
-        return 'received'
+        flash('Contact Added Successfully')
+        return redirect(url_for('Index'))
 
 @app.route('/edit')
 def edit_contact():
     return 'edit_contact'
 
-@app.route('/delete')
-def delete_contact():
-    return 'delete_contact'
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute(f'DELETE FROM contacts WHERE id = {id}')
+    mysql.connection.commit()
+    flash('Contact Removed Successfully')
+    return redirect(url_for('Index'))
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
